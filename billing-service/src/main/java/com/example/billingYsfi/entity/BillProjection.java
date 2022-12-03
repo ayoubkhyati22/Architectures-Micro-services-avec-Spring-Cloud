0c4ecf537a93d0b1c4a4fe89fdcb89e69b036323
ayoubkhyati22 <ayoubkhyati89@gmail.com>
2022-12-03 23:51:16 +0100

    Angular Client

---
+++ billing-service/src/main/java/com/example/billingYsfi/entity/BillProjection.java
@@ -0,0 +1,13 @@
+package com.example.billingYsfi.entity;
+
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.Date;
+
+@Projection(name="fullBill", types = Bill.class)
+public interface BillProjection {
+    Long getId();
+    Date getBillingDate();
+    Long getCustomerID();
+
+}
